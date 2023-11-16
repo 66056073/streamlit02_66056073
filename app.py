7631bfc065d7a479fb827bf60916f070f86bcf92
66056073 <66056073@kmitl.ac.th>
2023-11-16 19:55:25 +0700

    Change title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 
 st.markdown('สวัสดี! **Streamlit**')
-st.title('San Francisco Columns')
+st.title('San Francisco Trees Columns')
 st.write("""
 เราจะลองทำ San Francisco Dataset กันดู
 """)

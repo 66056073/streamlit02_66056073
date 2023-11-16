977a01a7fc103e049c2a85624d62bc13914b60d7
66056073 <66056073@kmitl.ac.th>
2023-11-16 20:19:17 +0700

    Set layout

--- pages/tab.py
+++ pages/tab.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout='wide')
+
+st.markdown('สวัสดี! **Streamlit**')
+st.title('San Francisco Trees Tab')
+st.write("""
+เราจะลองทำ San Francisco Dataset กันดู
+""")
+
+trees_df = pd.read_csv('trees.csv')

79a56bcebcbd33720a449bc40d29b63a0e17b544
66056073 <66056073@kmitl.ac.th>
2023-11-16 20:12:56 +0700

    Copy and edit from app.py

--- pages/map.py
+++ pages/map.py
@@ -2,3 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+st.set_page_config(layout='wide')
+
+st.markdown('สวัสดี! **Streamlit**')
+st.title('San Francisco Trees Map')
+st.write("""
+เราจะลองทำ San Francisco Dataset กันดู
+""")
+
+trees_df = pd.read_csv('trees.csv')
